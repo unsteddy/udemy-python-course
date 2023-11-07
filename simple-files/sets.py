fbcc6775df53167a27fd84c0894bd7e3c7b1010e
eddie <edwardryan@live.com>
2023-11-07 00:37:15 +0000

    add exercise

--- simple-files/sets.py
+++ simple-files/sets.py
@@ -10,3 +10,18 @@ print(not_in_both)
 
 all_friends = art_friends.union(science_friends)
 print(all_friends)
+
+
+
+# exercise
+nearby_people = {'Rolf', 'Jen', 'Anna'}
+user_friends = set()  # This is an empty set, like {}
+
+# Ask the user for the name of a friend
+friend_name = input("Yo gimme a friends name: ")
+
+# Add the name to the empty set
+user_friends.add(friend_name)
+
+# Print out the intersection between both sets. This gives us a set with those friends that are nearby.
+print(nearby_people.intersection(user_friends))
