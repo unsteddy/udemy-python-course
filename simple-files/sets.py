4f3a7802b581ecc88deaea5ea1d1daa1de43eb88
eddie <edwardryan@live.com>
2023-11-07 00:33:51 +0000

    sets differences and union

--- simple-files/sets.py
+++ simple-files/sets.py
@@ -1,8 +1,12 @@
-art_friends = {"Rolf", "Anne"}
+art_friends = {"Rolf", "Anne", "Jen"}
 science_friends = {"Jen", "Charlie"}
 
-art_friends.add("Jen")
-print(art_friends)
+art_but_not_science = art_friends.difference(science_friends)
+science_but_not_art = science_friends.difference(art_friends)
 
-art_friends.remove("Jen")
-print(art_friends)
+not_in_both = art_friends.symmetric_difference(science_friends)
+print(not_in_both)
+
+
+all_friends = art_friends.union(science_friends)
+print(all_friends)
