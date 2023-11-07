bb3c950ffda3653855ecf1ccb23703503c4c00ce
eddie <edwardryan@live.com>
2023-11-07 00:03:54 +0000

    user input lesson

---
+++ simple-files/booleans.py
@@ -0,0 +1,13 @@
+truthy = True
+falsy = False
+
+age = 20
+is_over_age = age >= 18
+is_under_age = age < 18
+is_twenty = age == 20
+
+my_number = 30
+user_number = int(input("Enter a number: "))
+matches = my_number == user_number
+
+print(f"You got it right: {matches}.")
